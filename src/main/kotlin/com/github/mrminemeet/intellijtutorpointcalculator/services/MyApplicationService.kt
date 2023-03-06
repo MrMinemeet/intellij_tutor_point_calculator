89f8a2691e3df37ec987e9c30d303ae50603c18f
GitHub Action <action@github.com>
2023-03-06 14:35:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/mrminemeet/intellijtutorpointcalculator/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mrminemeet/intellijtutorpointcalculator/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mrminemeet.intellijtutorpointcalculator.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mrminemeet.intellijtutorpointcalculator.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/mrminemeet/intellijtutorpointcalculator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mrminemeet/intellijtutorpointcalculator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mrminemeet.intellijtutorpointcalculator.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mrminemeet.intellijtutorpointcalculator.MyBundle
 
 class MyProjectService(project: Project) {
 
